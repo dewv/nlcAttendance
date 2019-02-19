26f4bc117fd2f0542fd3dac86e0dc4c2ca28a6c6
Steve Mattingly <mattinglys@dewv.edu>
2019-02-19 21:44:06 +0000

    Reorganize docs files

--- api/docs.js
+++ api/docs.js
@@ -1,6 +1,20 @@
 // Document symbols for jsdoc
 
 /**
+ * Represents a set of structured data, called records.
+ * @interface Model
+ * @see {@link https://sailsjs.com/documentation/concepts/models-and-orm/models|Sails Concepts}
+ * @see {@link https://sailsjs.com/documentation/reference/waterline-orm/models|Sails Reference}
+ */
+
+/**
+ * A uniquely identifiable object that corresponds 1-to-1 with a physical database entry.
+ * @interface Record
+ * @see {@link https://sailsjs.com/documentation/concepts/models-and-orm/records|Sails Concepts}
+ * @see {@link https://sailsjs.com/documentation/reference/waterline-orm/records|Sails Reference}
+ */
+
+/**
  * A dictionary of request-handling actions. 
  * @interface Controller
  * @see {@link https://sailsjs.com/documentation/concepts/actions-and-controllers#?controllers|Sails Concepts}
