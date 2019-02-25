c6f82bae12811e84a90781a63d8b4b0a345936d2
Ricard <castanr@dewv.edu>
2019-02-25 15:10:25 +0000

    Student test

--- test/models/Student.js
+++ test/models/Student.js
@@ -8,7 +8,6 @@ const helperIntegrationTests = require("./_helpers");
 const visitModelTest = require("./_visit");
 
 describe("Student model", function() {
-<<<<<<< HEAD
     let testData = {
         associations: {},
         record: {}
@@ -54,7 +53,7 @@ describe("Student model", function() {
 
     // Run helper integration tests
     helperIntegrationTests("student", testData);
-    
+
     //Run Visit model test
     visitModelTest("student", testData);
 
@@ -85,87 +84,3 @@ describe("Student model", function() {
         await Major.destroyOne({ name: testMajorTwo.name });
     }
 });
-=======
-        let testData = {
-            associations: {},
-            record: {}
-        };
-        
-        let testStudent = {
-            name: "TESTSTUDENT@DEWV.EDU",
-            firstName: "TEST",
-            lastName: "STUDENT",
-            academicRank: "Freshman",
-            residentialStatus: "On campus"
-        };
-        
-        let testFallSport = { name: "TEST FALL SPORT" };
-        let testSpringSport = { name: "TEST SPRING SPORT" };
-        let testMajorOne = { name: "TEST MAJOR ONE" };
-        let testMajorTwo = { name: "TEST MAJOR TWO" };
-        
-        // Before tests run ...
-        before(async function() {
-        
-           // Destroy any old test data that might be haning around
-            await destroyTestData();
-            
-            // Create test data: associated model records
-            testData.associations.fallSport = await FallSport.create(testFallSport).fetch();
-            testData.associations.springSport = await SpringSport.create(testSpringSport).fetch();
-            testData.associations.majorOne = await Major.create(testMajorOne).fetch();
-            testData.associations.majorTwo = await Major.create(testMajorTwo).fetch();
-            
-            // Associate, using the new IDs
-            testStudent.fallSport = testData.associations.fallSport.id;
-            testStudent.springSport = testData.associations.springSport.id;
-            testStudent.majorOne = testData.associations.majorOne.id;
-            testStudent.majorTwo = null;
-            
-            // Create main test record, with associations in place
-            testData.record = await Student.create(testStudent).fetch();
-        });
-        
-        // After tests run ...
-        after(destroyTestData);
-        
-        // Run helper integration tests
-        helperIntegrationTests("student", testData);
-        //Run Visit model test
-        visitModelTest("student", testData);
-        
-        
-        async function destroyTestData() {
-            // Destroy main test record
-            await Student.destroyOne({ name: testStudent.name });
-            
-            
-            context("A data record returned by `create()`", function() {
-                it("should have a field for each model attribute", function() {
-                    for (let property in testStudent) {
-                        testData.record.should.have.property(property);
-                    }
-                    
-                    for (let property in testData.associations) {
-                        testData.record.should.have.property(property);
-                    }
-                });
-                
-                it("should have correct default field values", function() {
-                    testData.record.forceUpdate.should.be.true();
-                });
-            });
-            
-            async function destroyTestData() {
-                // Destroy main test record
-                await Student.destroyOne({ username: testStudent.username });
-             
-                // Destroy test records in associated model
-                await FallSport.destroyOne({ name: testFallSport.name });
-                await SpringSport.destroyOne({ name: testSpringSport.name });
-                await Major.destroyOne({ name: testMajorOne.name });
-                await Major.destroyOne({ name: testMajorTwo.name });
-            };
-        };
-    };
->>>>>>> 8443181b68be2ab0a5654d1d85ee55cca42fc9b2
