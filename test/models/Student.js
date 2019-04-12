aee1b4650b24ec4f702dcee0db48be885bde5bd1
Cody Broyles <broylesc@dewv.edu>
2019-04-12 17:30:29 +0000

    removed commented out line from student test. staff model is still lacking in coverage.

--- test/models/Student.js
+++ test/models/Student.js
@@ -4,7 +4,6 @@
 
 require("should");
 const helperIntegrationTests = require("./_helpers");
-// const visitModelTest = require("./Visit");
 
 describe("Student model", function() {
     let testData = {
@@ -16,7 +15,7 @@ describe("Student model", function() {
     before(async function() {
 
         // Create main test record, with associations in place
-        testData.record = Student.testRecords[1]; //await Student.create(testStudent).fetch();
+        testData.record = Student.testRecords[1];
         testData.associations.fallSport = await FallSport.findOne({ id: testData.record.fallSport });
         testData.associations.springSport = await SpringSport.findOne({ id: testData.record.springSport });
         testData.associations.majorOne = await Major.findOne({ id: testData.record.majorOne });
