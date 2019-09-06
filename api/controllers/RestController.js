5ec53e0d5c24165781d70b7b7f2f7b156f1437b7
Cody Broyles <broylesc@dewv.edu>
2019-09-06 17:26:00 -0400

    Can't figure out the error i am seeing.

--- api/controllers/RestController.js
+++ api/controllers/RestController.js
@@ -15,7 +15,7 @@ module.exports = {
         if (request.params.model === "controller") return response.cookie("RestController", "listRequested").end();
         let model = sails.models[request.params.model];
         if (!model) return response.notFound();
-        let records = await model.find( { sort: " id DESC"} ).populate('studentID');
+        let records = await model.find().populate('studentId');
         clearRestCookies(response);
         return sails.helpers.responseViewSafely(request, response, `pages/${request.params.model}/index`, { records: records });
     },
