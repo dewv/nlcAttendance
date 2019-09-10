35b7dbb7bc12c688a6f760032593d9b2ba77f258
Cody Broyles <broylesc@dewv.edu>
2019-09-10 11:38:03 -0400

    Created populateAssociations Helper to direct list queries to the model to be handle it's associations.

---
+++ api/helpers/populate-associations.js
@@ -0,0 +1,43 @@
+/**
+ * @name sails&period;helpers&period;populateAssociations
+ * @description Joins all data association ID's with their corresponding records.
+ * @function
+ * @argument {Model} model - A Sails model defining the associations, if any.
+ * @argument {Records} records - A data record that may contain association domain values that need encoding.
+ * @return {Records} The record argument, modified so that any association domain values are replace with their record.
+ * @async
+ */
+module.exports = {
+    friendlyName: "Populate associations",
+
+    description: "Replaces all model association ID's with their corresponding records",
+
+    inputs: {
+        model: {
+            description: "A Sails model defining the associations, if any.",
+            type: "ref",
+            required: true
+        },
+
+        records: {
+            description: "A array of records that may contain association domain values that need encoding.",
+            type: "ref",
+            required: true
+        }
+    },
+
+    exits: {
+        success: {
+            outputFriendlyName: "Populated associations",
+        }
+    },
+
+    fn: async function(inputs, exits) {
+        if (inputs.model.afterPopulateAssociations) {
+            await inputs.model.afterPopulateAssociations(inputs.records);
+        }
+        // Send back the result through the success exit.
+        return exits.success(inputs.records);
+    }
+
+};
