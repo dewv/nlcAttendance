295a83c37683665b200246709bbb681cf0bdf6f7
Cody Broyles <broylesc@dewv.edu>
2019-10-07 11:46:11 -0400

    removed password from mysql

--- provision.sh
+++ provision.sh
@@ -6,7 +6,7 @@ sudo apt-get update
 sudo apt-get install -y mysql-server
 sudo service mysql status
 sudo service mysql start
-sudo mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'RootPassword';"
+sudo mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';"
 sudo apt-get install -y nodejs
 sudo apt-get install -y npm 
 sudo npm install sails -g
