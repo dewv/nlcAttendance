fbdb24bf3940ce38b1112e58c67fc5e37787a78d
Steve Mattingly <mattinglys@dewv.edu>
2019-10-27 00:59:57 -0400

    Improve auto logout

--- assets/js/logoutTimer.js
+++ assets/js/logoutTimer.js
@@ -1,15 +1,15 @@
-const ONE_MINUTE =  1000;
+const ONE_MINUTE =  60 * 1000;
 var logoutTime;
 
 function setLogoutTimer() { // eslint-disable-line no-unused-vars
     var popup = document.getElementById("logoutWarning");
 
     var logoutWarning = function () {
-        if (popup) popup.style.visibility = "visible";
-        logoutTime = setTimeout("console.log('times up'); window.location = '/logout';", ONE_MINUTE);
+        if (popup) popup.style.display = "block";
+        logoutTime = setTimeout("window.location = '/logout';", ONE_MINUTE);
     };
 
-    if (popup) popup.style.visibility = "hidden";
+    if (popup) popup.style.display = "none";
     clearTimeout(logoutTime);
     setTimeout(logoutWarning, 5 * ONE_MINUTE);
 }
