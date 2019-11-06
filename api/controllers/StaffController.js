e59e8cbb4276aa6a296dc937edbddc1b1eedaaec
Cody Broyles <broylesc@dewv.edu>
2019-11-06 11:31:36 -0500

    Removed major and sports model code.

--- api/controllers/StaffController.js
+++ api/controllers/StaffController.js
@@ -7,6 +7,7 @@
 const RestController = require("./RestController");
 
 module.exports = {
+    
     /**
      * Handles request to update a data record using form data.
      * @argument {external:Request} request -  The HTTP request.
@@ -20,57 +21,6 @@ module.exports = {
         response.locals.banner = "Your staff profile was updated.";
         request.session.Url = request.session.defaultUrl;
         request.params.model = "staff";
-        return RestController.editFormSubmitted(request, response);
-    },
-
-    /**
-     * Handles request to update or create a major data record using form data.
-     * @argument {external:Request} request -  The HTTP request.
-     * @argument {external:Response} response - The HTTP response.
-     * @public
-     * @async
-     */
-    majorFormSubmitted: async function (request, response) {
-        if (request.params.model === "controller") return response.cookie("StaffController", "majorFormSubmitted").end();
-        // If action is a create
-        if (request.path === "/major") {
-            request.session.Url = "/major";
-            request.params.model = "major";
-            return RestController.createFormSubmitted(request, response);
-        }
-        // If action is a edit
-        request.session.Url = "/major";
-        request.params.model = "major";
-        return RestController.editFormSubmitted(request, response);
-    },
-
-    /**
-     * Handles request to update or create a sport data record using form data.
-     * @argument {external:Request} request -  The HTTP request.
-     * @argument {external:Response} response - The HTTP response.
-     * @public
-     * @async
-     */
-    sportFormSubmitted: async function (request, response) {
-        if (request.params.model === "controller") return response.cookie("StaffController", "sportFormSubmitted").end();
-        request.session.Url = "/sports";
-        // If action is a create on spring sport
-        if (request.path === "/springsport") {
-            request.params.model = "springsport";
-            return RestController.createFormSubmitted(request, response);
-        }
-        // If action is a create on fall sport 
-        if (request.path === "/fallsport") {
-            request.params.model = "fallsport";
-            return RestController.createFormSubmitted(request, response);
-        }
-        // If action is a edit on spring sport
-        if (request.route.path === "/springsport/:id") {
-            request.params.model = "springsport";
-            return RestController.editFormSubmitted(request, response);
-        }
-        // If action is a edit on fall sport
-        request.params.model = "fallsport";
         return RestController.editFormSubmitted(request, response);
     },
 
