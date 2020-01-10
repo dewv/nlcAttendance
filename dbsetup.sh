eb03a8193b14da9702b61da66ec5ea73b0fe802d
Steve Mattingly <mattinglys@dewv.edu>
2020-01-10 16:18:27 -0500

    Commit/push from bash because Code dropped execute permissions

--- dbsetup.sh
+++ dbsetup.sh
@@ -4,3 +4,4 @@ mysql -u root -e "CREATE DATABASE IF NOT EXISTS nlcAttendance;"
 mysql -u root -e "CREATE USER IF NOT EXISTS 'nlc'@'localhost' IDENTIFIED BY 'password';" 
 mysql -u root -e "GRANT SELECT, INSERT, UPDATE, CREATE, DROP ON nlcAttendance.* TO 'nlc'@'localhost';" 
 mysql -u root -e "FLUSH PRIVILEGES;"
+
