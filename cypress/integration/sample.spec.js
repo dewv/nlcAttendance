a4973d0863b5ce8c592ac4e6ab3cd77e31f89e31
Steve Mattingly <mattinglys@dewv.edu>
2020-12-12 20:57:09 +0000

    More cypress setup

---
+++ cypress/integration/sample.spec.js
@@ -0,0 +1,9 @@
+describe('My First Test', () => {
+    it('Gets, types and asserts', () => {
+        cy.visit('/');
+        // Get an input, type into it and verify that the value has been updated
+        cy.get('[name="username"]')
+          .type('fake@email.com')
+          .should('have.value', 'fake@email.com')
+    })
+})
