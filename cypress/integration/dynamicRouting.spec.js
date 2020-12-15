49cd35e8f78927c28ebd556cf3b5d2c778929d59
Steve Mattingly <mattinglys@dewv.edu>
2020-12-15 03:39:00 +0000

    Improve comments

--- cypress/integration/dynamicRouting.spec.js
+++ cypress/integration/dynamicRouting.spec.js
@@ -1,9 +1,9 @@
 describe("Dynamic routing for students", () => {
     it("Uses student status to display the correct page", () => {
         function _submitLoginForm(username, password) {
-            cy.get('[name="username"]').type(username);
-            cy.get('[name="password"]').type(password);
-            cy.get('[data-cy=submit]').click();
+            cy.get("[name=\"username\"]").type(username);
+            cy.get("[name=\"password\"]").type(password);
+            cy.get("[data-cy=submit]").click();
         }
 
         // Guarantee first-time student user
@@ -14,28 +14,30 @@ describe("Dynamic routing for students", () => {
         cy.visit("/");
         _submitLoginForm(username, password);
 
-        // ... sould require student profile update.
-        cy.url().should('match', /\/student\/.*\/edit$/);
+        // ... should require student profile update.
+        cy.url().should("match", /\/student\/.*\/edit$/);
         // TODO user can submit form doing nothing - no required entries!
-        cy.get('[data-cy=submit]').click();
+        cy.get("[data-cy=submit]").click();
 
-        // Then, sessions alternate between checking in ...
-        cy.url().should('match', /\/visit\/new$/);
+        // Then, student should check in.
+        cy.url().should("match", /\/visit\/new$/);
         const purpose = "Try to take over the world!";
-        cy.get('[name="purpose"]').type(purpose);
-        cy.get('[data-cy=submit]').click();
-        cy.url().should('match', /\/logout$/);
+        cy.get("[name=\"purpose\"]").type(purpose);
+        cy.get("[data-cy=submit]").click();
+        // (Check in or our automatically logs them out.)
+        cy.url().should("match", /\/logout$/);
 
-        // ... checking out, ...
+        // Next time they should check out.
         _submitLoginForm(username, password);
-        cy.url().should('match', /\/visit\/.*\/edit$/);
-        cy.get('[name="purposeAchieved"]').select('Yes');
-        cy.get('[name="usedTutor"]').select('No');
-        cy.get('[data-cy=submit]').click();
-        cy.url().should('match', /\/logout$/);
+        cy.url().should("match", /\/visit\/.*\/edit$/);
+        cy.get("[name=\"purposeAchieved\"]").select("Yes");
+        cy.get("[name=\"usedTutor\"]").select("No");
+        cy.get("[data-cy=submit]").click();
+        // (Check in or our automatically logs them out.)
+        cy.url().should("match", /\/logout$/);
 
-        // ... and back in again.
+        // And next time, check in again.
         _submitLoginForm(username, password);
-        cy.url().should('match', /\/visit\/new$/);
+        cy.url().should("match", /\/visit\/new$/);
     });
 });
