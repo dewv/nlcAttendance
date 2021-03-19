c056a0e1b0f4ffa03696875d92fbb474b72b7410
Steve Mattingly <mattinglys@dewv.edu>
2021-03-19 16:51:45 +0000

    Spec pending tests

---
+++ cypress/integration/visitIndex.spec.js
@@ -0,0 +1,8 @@
+describe("The visits index page", () => {
+    it("should display a list of all student visits" /*, () => {
+        const username = `visitIndexPage${(new Date()).valueOf()}`;
+        cy.loginStaff(username);
+    } */ );
+
+    it("should allow a CSV download of all student visit data");
+});
