bdb4453503663ad51b9a83174dc6493b89ac98de
jacobj1234 <johnsonj6@dewv.edu>
2021-04-19 14:49:21 +0000

    Added discontinue A major code

--- cypress/integration/manageMajors.spec.js
+++ cypress/integration/manageMajors.spec.js
@@ -13,4 +13,21 @@ describe("Data management for Majors", () => {
     });
 
     it("should allow staff to change a Major's `discontinued` flag."); // TODO
+    cy.loginStaff("DiscontinueAMajor");
+
+    cy.get("[data-cy=majors-link]").click();
+    let openBoxes = cy.get('.fa-squares-o')
+    let openBoxesCount = openBoxes.length
+
+    cy.url().should("match", /\/major\/new$/);
+    let openBoxes = cy.get('.fa-square-0')
+    let checkedBoxesCount = openBoxes.length
+    cy.get("[name=name]").type("Discontinue major");
+    openBoxes[0].click();
+    cy.get('.fa-square-o').length.should.equal(openBoxCount - 1);
+    cy.get("[data-cy=submit]").click();
+    checkedBoxes[0].click();
+    cy.get('.fa-squares-o').length.should.equal(checkedBoxesCount + 1);
+    cy.url().should("match", /\/major$/);
+
 });
