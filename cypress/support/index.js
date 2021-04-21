211f263dd6a01bd48aebfc3815dd3efe857ef9e9
Jakob Makarenko <makarenkoj@dewv.edu>
2021-04-21 15:41:45 +0000

    Login form tests

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -19,13 +19,24 @@
 // Alternatively you can use CommonJS syntax:
 require("./commands");
 
+Cypress.Commands.add("loginUnathorized", (username, navigate) => {
+    if (navigate !== false) cy.visit("/login");
+    cy.get("[name=username]").type(username);
+    cy.get("[name=password]").type("neither");
+    cy.get("[data-cy=submit]").click();
+});
 Cypress.Commands.add("loginStaff", (username, navigate) => {
     if (navigate !== false) cy.visit("/login");
     cy.get("[name=username]").type(username);
     cy.get("[name=password]").type("staff");
     cy.get("[data-cy=submit]").click();
 });
-
+Cypress.Commands.add("loginNoLdap", (username, navigate) => {
+    if (navigate !== false) cy.visit("/login");
+    cy.get("[name=username]").type(username);
+    cy.get("[name=password]").type("noldap");
+    cy.get("[data-cy=submit]").click();
+});
 Cypress.Commands.add("loginStudent", (username, navigate) => {
     if (navigate !== false) cy.visit("/login");
     cy.get("[name=username]").type(username);
