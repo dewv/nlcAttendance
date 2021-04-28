ee72922ea0a649bd8f33c3661de5c3efe1cc81c7
BroadusA <broadusa@dewv.edu>
2021-04-28 14:27:56 +0000

    Visit index

--- cypress/integration/visitIndex.spec.js
+++ cypress/integration/visitIndex.spec.js
@@ -1,8 +1,18 @@
 describe("The visits index page", () => {
-    it("should display a list of all student visits" /*, () => {
+    it("should display a list of all student visits", () => {
         const username = `visitIndexPage${(new Date()).valueOf()}`;
-        cy.loginStaff(username);
-    } */ );
+        cy.getloginStaff(username);
+
+        // maybe just scroll to bottom of table and test there are 30 rows
+        cy.get("tr").should("have.length", 10);
+        cy.get(".sidebar").scrollTo("bottom");
+        cy.get("tr").should("have.length", 30);
+
+
+
+
+    });
 
     it("should allow a CSV download of all student visit data");
 });
+
