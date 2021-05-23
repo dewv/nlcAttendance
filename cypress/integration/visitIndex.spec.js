7bf331933b2f365bc51db3b41058d8a64ee0fe08
Steve Mattingly <mattinglys@dewv.edu>
2021-05-23 22:12:17 +0000

    Fix visit list test

--- cypress/integration/visitIndex.spec.js
+++ cypress/integration/visitIndex.spec.js
@@ -1,16 +1,13 @@
 describe("The visits index page", () => {
-    it("should display a list of all student visits", () => {
+    it("should display student visits with fetch-on-scroll", () => {
         const username = `visitIndexPage${(new Date()).valueOf()}`;
-        cy.getloginStaff(username);
+        cy.loginStaff(username);
 
-        // maybe just scroll to bottom of table and test there are 30 rows
-        cy.get("tr").should("have.length", 10);
-        cy.get(".sidebar").scrollTo("bottom");
-        cy.get("tr").should("have.length", 30);
-
-
-
-
+        cy.get("tbody > tr").should("have.length", 10);
+        cy.get("#infinite-scroller").scrollTo("bottom");
+        cy.get("tbody > tr").should("have.length", 20);
+        cy.get("#infinite-scroller").scrollTo("bottom");
+        cy.get("tbody > tr").should("have.length", 30);
     });
 
     it("should allow a CSV download of all student visit data");
