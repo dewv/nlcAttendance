4dd994cd08b21586034c4ecf9144deeb1e2b2c12
Steve Mattingly <mattinglys@dewv.edu>
2021-05-25 02:15:36 +0000

    Fix custom check box and tests

---
+++ assets/js/toggleCustomCheckbox.js
@@ -0,0 +1,12 @@
+function toggleCustomCheckbox(url, rowNum) {
+    let discontinued = document.querySelector(`#name${rowNum}`).classList.toggle("discontinued") ? "Yes" : "No";
+    document.querySelector(`#toggle${rowNum} i`).classList.toggle("fa-check-square-o");
+    document.querySelector(`#toggle${rowNum} i`).classList.toggle("fa-square-o");
+
+    let formData = new FormData();
+    formData.append("discontinued", discontinued);
+    fetch(url, {
+        method: "POST",
+        body: new URLSearchParams([...formData])
+    });
+}
